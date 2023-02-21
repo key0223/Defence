559b1cef8c76788a33640e67e802ae25d8313a2c
addkey <addkey0223@gmail.com>
2023-02-21 19:02:15 +0900

    Create MoneyUI.cs

---
+++ Scripts/UI/MoneyUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MoneyUI : MonoBehaviour
+{
+    public TextMeshProUGUI moneyText;
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text = PlayerStats.money.ToString();
+    }
+}
