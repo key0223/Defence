41623e4dfc0d2ecc865f250e13b9396860788ccc
addkey <addkey0223@gmail.com>
2023-02-21 19:01:23 +0900

    Update PlayerStats.cs

--- Scripts/Managers/PlayerStats.cs
+++ Scripts/Managers/PlayerStats.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public static int money;
-    public int startMoney = 0;
+    public int startMoney = 400;
+
+    public static int lives;
+    public int startLives = 20;
+
 
     private void Start()
     {
         money = startMoney;
+        lives= startLives;
     }
+
 }
