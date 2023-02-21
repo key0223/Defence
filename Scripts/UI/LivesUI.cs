6f8d065bec7ac67c6e7c7bfa0c6c5ce7fa788ecb
addkey <addkey0223@gmail.com>
2023-02-21 19:02:04 +0900

    create livesUI

---
+++ Scripts/UI/LivesUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LivesUI : MonoBehaviour
+{
+    public TextMeshProUGUI livesText;
+
+
+    void Update()
+    {
+        livesText.text= PlayerStats.lives.ToString();
+    }
+}
