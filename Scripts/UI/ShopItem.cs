de9db3ee1b8b026b80aa9e0a30b175a4fc7fe56c
addkey <addkey0223@gmail.com>
2023-02-21 19:02:39 +0900

    Create ShopItem.cs

---
+++ Scripts/UI/ShopItem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItem : MonoBehaviour
+{
+    Button button;
+
+    Shop shop;
+
+    [SerializeField]
+    public WeaponType weaponType;
+    [SerializeField]
+    public int cost;
+    [SerializeField]
+    TextMeshProUGUI costText;
+
+    private void Awake()
+    {
+        shop= FindObjectOfType<Shop>();
+        button= GetComponent<Button>();
+        button.onClick.AddListener(ItemClicked);
+
+        costText.text = cost.ToString();
+    }
+
+    public void ItemClicked()
+    {
+        shop.Selected(this);
+    }
+
+}
