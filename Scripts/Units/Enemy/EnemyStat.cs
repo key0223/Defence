a22e662bbe244e609306513264d47dc11e50dd48
addkey <addkey0223@gmail.com>
2023-02-21 19:03:37 +0900

    Update EnemyStat.cs

--- Scripts/Units/Enemy/EnemyStat.cs
+++ Scripts/Units/Enemy/EnemyStat.cs
@@ -4,7 +4,37 @@ using UnityEngine;
 
 public class EnemyStat : MonoBehaviour
 {
+    EnemyPooler enemyPooler;
+    EffectPooler effectPooler;
     public EnemyType enemyType;
-    public float hp;
+    public EffectType effectType;
+    public float hp = 100f;
     public float speed;
+
+    private void Awake()
+    {
+        enemyPooler= FindObjectOfType<EnemyPooler>();
+        effectPooler= FindObjectOfType<EffectPooler>();
+    }
+    public void TakeDamege(float amount)
+    {
+        Debug.Log("Take Damage");
+        hp -= amount;
+
+        if(hp <= 0)
+        {
+            Die();
+        }
+
+    }
+
+     void Die()
+    {
+        GameObject deathEffect = effectPooler.GetEffect(effectType);
+        deathEffect.transform.position = transform.position;
+        deathEffect.transform.rotation = Quaternion.identity;
+        deathEffect.SetActive(true);
+
+        enemyPooler.ExpiredEnemy(gameObject);
+    }
 }
